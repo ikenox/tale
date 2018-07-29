4d863ce01f1ffa7245444e3e972c7ba4f4fb144b
Naoto Ikeno <ikenox@gmail.com>
2018-07-30 01:03:38 +0900

    update

--- ikenox-info/src/templates/blog-post.js
+++ ikenox-info/src/templates/blog-post.js
@@ -3,41 +3,60 @@ import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
 import Bio from '../components/Bio'
+import styled from 'styled-components';
 
 require('./blog-post.css')
+
+const Pager = styled.div`
+a {
+  text-decoration:none;
+}
+
+.linkPrevious{
+font-size:.9em;
+padding:5px;
+}
+.linkNext{
+font-size:.9em;
+padding:5px;
+}
+`
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pathContext
+    const {previous, next} = this.props.pathContext
 
     return (
       <div>
-        <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-        <h1 style={{ marginBottom:"0px" }}>{post.frontmatter.title}</h1>
-        <p style={{ marginTop:"0px", fontWeight:"bold", color:"#777" }}>{post.frontmatter.date}</p>
-        <div className={`content`} dangerouslySetInnerHTML={{ __html: post.html }} style={{ marginTop:"48px" }} />
-        <hr />
-        <Bio />
+        <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
+        <h1 style={{marginBottom: "0px"}}>{post.frontmatter.title}</h1>
+        <p style={{marginTop: "0px", fontWeight: "bold", color: "#666"}}>{post.frontmatter.date}</p>
+        <div className={`content`} dangerouslySetInnerHTML={{__html: post.html}}
+             style={{marginTop: "48px", marginBottom: "48px"}}/>
+        <hr/>
+        <Bio/>
 
-        <ul>
+        <hr/>
+        <Pager>
           {previous && (
-            <li>
+            <div className={`linkPrevious`}>
               <Link to={previous.fields.slug} rel="prev">
                 ← {previous.frontmatter.title}
               </Link>
-            </li>
+            </div>
           )}
 
           {next && (
-            <li>
+            <div className={`linkNext`}>
               <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
+                → {next.frontmatter.title}
               </Link>
-            </li>
+            </div>
           )}
-        </ul>
+        </Pager>
+        <hr/>
       </div>
     )
   }
