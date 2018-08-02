6783332d716a2ffd4af976c8873c046a9cdad066
Naoto Ikeno <ikenox@gmail.com>
2018-08-02 22:41:16 +0900

    hidden social button placeholder link

--- ikenox-info/src/components/SocialButtons.js
+++ ikenox-info/src/components/SocialButtons.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { HatenabookmarkButton, PocketButton, FacebookButton, TwitterTweetButton } from 'react-social-sharebuttons';
 
+import styled from 'styled-components';
+
+const Div = styled.div`
+  a { display:none; }
+`
+
 class SocialButtons extends React.Component {
   render() {
     return (
-      <div style={{ verticalAlign:"middle" }}>
+      <Div style={{ verticalAlign:"middle" }}>
         <HatenabookmarkButton layout={`standard-balloon`}/>
         <FacebookButton layout={`button_count`} share={true} style={{ margin:"5px" }}/>
         <TwitterTweetButton/>
         <PocketButton count={`horizontal`}/>
-      </div>
+      </Div>
     )
   }
 }
