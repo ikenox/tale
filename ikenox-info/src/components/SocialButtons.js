e81d54b205a39123cb14777300fae276b73b7041
Naoto Ikeno <ikenox@gmail.com>
2018-08-02 23:18:55 +0900

    update

--- ikenox-info/src/components/SocialButtons.js
+++ ikenox-info/src/components/SocialButtons.js
@@ -5,24 +5,24 @@ import styled from 'styled-components';
 
 const Div = styled.div`
   a { display:none; }
-  > div { display:inline-block; margin-right:5px; }
+  > div { display:inline-block; margin-right:6px; }
 `
 
 class SocialButtons extends React.Component {
   render() {
     return (
-      <Div style={{height:"30px"}}>
-        <div style={{width: "115px", height: "30px !important"}}>
+      <Div style={{height:"25px"}}>
+        <div style={{width: "115px", height: "25px !important"}}>
           <HatenabookmarkButton layout={`standard-balloon`}/>
         </div>
-        <div style={{width: "106px", height: "30px !important"}}>
-          <FacebookButton layout={`button_count`} share={true} style={{margin: "5px"}}/>
+        <div style={{width: "106px", height: "25px !important"}}>
+          <FacebookButton layout={`button_count`} share={true}/>
         </div>
-        <div style={{width: "61px", height: "30px !important"}}>
-          <TwitterTweetButton style={{ position:"absolute"}}/>
+        <div style={{width: "61px", height: "25px !important"}}>
+          <TwitterTweetButton/>
         </div>
-        <div style={{width: "135px", height: "30px !important"}}>
-          <PocketButton count={`horizontal`} style={{ position:"absolute"}}/>
+        <div style={{width: "135px", height: "25px !important"}}>
+          <PocketButton count={`horizontal`}/>
         </div>
       </Div>
     )
